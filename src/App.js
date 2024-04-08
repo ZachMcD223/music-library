ca23ec8f55ef76036119050c17b7caeb7bf19bd7
ZachMcD223 <148635518+ZachMcD223@users.noreply.github.com>
2024-04-08 13:04:21 -0500

    added album and artist views

--- src/App.js
+++ src/App.js
@@ -1,42 +1,69 @@
-import { useState, useEffect } from "react"
+import { useState, useRef, Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import SearchBar from "./Components/SearchBar";
 import Gallery from "./Components/Gallery";
-
+import { DataContext} from "./context/DataContext";
+import { SearchContext } from "./context/SearchContext";
+import AlbumView from "./Components/AlbumView";
+import ArtistView from "./Components/ArtistView";
 
 function App() {
   const [search, setSearch] = useState('')
   const [message, setMessage] = useState('Search for Music!')
   const [data, setData] = useState([])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const url = encodeURI(`https://itunes.apple.com/search?term=${search}`)
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-
-      if (data.results.length) {
-        setData(data.results)
-      } else {
-        setMessage('Not Found')
-      }
-    }
-
-   if (search) fetchData()
-  }, [search])
+  let searchInput = useRef('')
 
   const handleSearch = async (e, term) => {
     e.preventDefault()
-    setSearch(term)
+    const fetchData = async () => {
+      const url = encodeURI(`https://itunes.apple.com/search?term=${term}`);
+      document.title = `${term} Music`
+      const response = await fetch(url);
+       const data = await response.json();
+      console.log(data);
+
+      if (data.results.length) {
+             setData(data.results)
+             } else {
+              setMessage('Not Found')
+             }
+     } 
+     if (term) fetchData()
+            
+          
   }
 
   return (
-    <div>
+    <div className="App">
+      <SearchContext.Provider value={ {term: searchInput, handleSearch: handleSearch} }>
       <SearchBar handleSearch={handleSearch} />
+      </SearchContext.Provider>
       {message}
-      <Gallery data={data}/>
+      <DataContext.Provider value={data}>
+      <Gallery/>
+      </DataContext.Provider>
+      <AlbumView />
+      <ArtistView />
     </div>
   );
-}
 
+
+return (
+  <div>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <Fragment>
+            <SearchBar handleSearch = {handleSearch} />
+            <Gallery data={data} />
+          </Fragment>
+        } />
+        <Route path="/album/:id" element={<AlbumView />} />
+        <Route path="/artist/:id" element={<ArtistView />} />
+      </Routes>
+    </Router>
+  </div>
+)
+}
 export default App;
